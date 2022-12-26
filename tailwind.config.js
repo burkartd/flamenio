a74df0d35ef1fe9cd3ff12b794ebf659f6cb35f0
tom-kroupa <64896728+tom-kroupa@users.noreply.github.com>
2022-12-26 18:11:42 +0100

    h

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./public/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#051A24',
+        blu: '#087E8B',
+        rosen: '#BB1018',
+        whiteb: '#F0F5FA'
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+    },
   },
   plugins: [],
 }
