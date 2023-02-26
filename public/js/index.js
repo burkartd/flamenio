afb18a07e559e9b779196220065c3a67485ffd9b
Daniel Burkart <burkartd03@gmail.com>
2023-02-26 21:17:17 +0100

    pokrok

--- public/js/index.js
+++ public/js/index.js
@@ -22,8 +22,88 @@ index1continue.addEventListener('click', ()=> {
         idInput.focus();
         return;
     }
+
+    //zkontrolovat jestli roomka existuje
+
      if(document.getElementById('lang_select').value === "0") return;
+
+     PatientData.language = document.getElementById('lang_select').value;
+     PatientData.RoomID = input;
     Zviditelnit(2);
 });
-index2continue.addEventListener('click', ()=> {Zviditelnit(3)});
+index2continue.addEventListener('click', ()=> {
+    
+    //jméno
+    console.log("dvojka")
+    let input = nameInput.value.trim();
+    console.log(input);
+    if(input.length < 1)
+    {
+        nameInput.value = "";
+        nameInput.focus();
+        return;
+    }
+    PatientData.Name = input;
+    //příjmení
+    input = surnameInput.value.trim();
+    console.log(input);
+
+    if(input.length < 1)
+    {
+        surnameInput.value = "";
+        surnameInput.focus();
+        return;
+    }
+    PatientData.Surname = input;
+    //datum
+    input = birthdayInput.value.trim();
+    console.log(input);
+
+    if(input.length < 1)
+    {
+        birthdayInput.value = "";
+        birthdayInput.focus();
+        return;
+    }
+    PatientData.Birthday = input;
+
+    input = countryInput.value;
+    console.log(input);
+
+    if(input === "0")
+    {
+        countryInput.focus();
+        return;
+    }
+    PatientData.Country = input;
+
+    PatientData.HasInsurance = document.getElementById('default-radio-2').checked;
+    
+    console.log(PatientData.HasInsurance);
+
+    VyplnIndex3()
+    Zviditelnit(3)});
+
+
+    function VyplnIndex3()
+    {
+        fieldNameSurname.innerHTML = PatientData.Name + " " + PatientData.Surname;
+        fieldBirthday.innerHTML = PatientData.Birthday;
+        fieldCountry.innerHTML = PatientData.Country;
+    }
+
+    chatIcon.addEventListener('click', ()=>{
+        
+        console.log('afdaewf')
+        Zviditelnit(4);
+    })
+
+    closeButton.addEventListener('click', ()=>{
+        Zviditelnit(3);
+    })
+
+    chatIcon.click();
+
+
+
 loginToDashboard.addEventListener('click', ()=>{window.location.href = '/prihlaseni_nemocnice.html?'});
